6bc6f31b944ab6c468e623eaaba543106cfaa138
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-05-13 18:57:09 +0530

    Update fast2.js

--- js/fast2.js
+++ js/fast2.js
@@ -34,7 +34,7 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
         request.open("post", url);
         request.send();
          //  replace
-            var wnd = window.open("https://www.fast2sms.com/dev/bulk?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&sender_id=FSTSMS&message=https://bestonewaytaxis.com%0AContact No: 80000900%0AHello "+strText+",%0AThank you for Booking with Best OneWay Taxi.Your "+strText2+" Booking has been Confirmed on "+strText4+".&language=english&route=p&numbers="+strText1);
+            var wnd = window.open("https://www.fast2sms.com/dev/bulk?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&sender_id=FSTSMS&message=https://bestonewaytaxis.com%0AContact No: 9994451282%0AHello "+strText+",%0AThank you for Booking with Best OneWay Taxi.Your "+strText2+" Booking has been Confirmed on "+strText4+".&language=english&route=p&numbers="+strText1);
 		//var wnd1=location.replace("bc.html","_self ");		      
                               if(wnd){
                                       setTimeout(function () { wnd.close();}, 1800);
